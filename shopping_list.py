f05801afdad0afccb1d05e9df085eefe38672aa2
bklynate <nathanielcfa@gmail.com>
2015-01-31 10:46:13 -0500

    Re worked the code using functions

--- shopping_list.py
+++ shopping_list.py
@@ -1,17 +1,37 @@
 shopping_list = []
 
-print("What should we pick up at the store? ")
-print("Enter 'DONE' to stop adding items.")
+def prompt(words):
+	print ("=> {}".format(words))
+
+def show_welcome():
+	prompt ("What should we pick up at the store? ")
+	prompt ("Enter 'DONE' to stop.")
+
+def show_list():
+	prompt("Here is your shopping list")
+	for items in shopping_list:
+		prompt (items)
+
+def add_to_list(items):
+	shopping_list.append(items)
+	prompt ("Added! Shopping list has {} items.".format(len(shopping_list)))	
+
+
+show_welcome()
 
 while True:
-	new_item = input("> ")
+	new_items = input("=> ")
 
-	if new_item == "DONE":
+	if new_items == "DONE":
 		break
+	add_to_list(new_items.capitalize())
+	continue
 
-	shopping_list.append(new_item)
-	print("Added! Shopping list has {} items.".format(len(shopping_list)))
-	
-print("Here is your list: ")
-for items in shopping_list:
-	print(items)
+show_list()
+
+
+
+
+
+
+
