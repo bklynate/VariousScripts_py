32421879a31b2a17144a7f5cfb33158b42ba2a44
bklynate <nathanielcfa@gmail.com>
2015-01-31 20:50:58 -0500

    Added functions for 'HELP' and to 'SHOW' current list at any moment

--- shopping_list.py
+++ shopping_list.py
@@ -1,3 +1,8 @@
+#1. Display instructions on how the "shopping list" works
+#2. Have the ability to add items to the shopping
+#3. Have the ability to show the current list
+#4. Have the ability to print numbered list at the CLOSE of program
+
 shopping_list = []
 
 def prompt(words):
@@ -5,12 +10,17 @@ def prompt(words):
 
 def show_welcome():
 	prompt ("What should we pick up at the store? ")
-	prompt ("Enter 'DONE' to stop.")
+	prompt ("Enter 'DONE' to stop. 'HELP' to display directions. 'SHOW' to show current list.")
+
+def show_help():
+	prompt ("Enter 'DONE' to stop. 'HELP' to display directions. 'SHOW' to show current list.")
 
 def show_list():
-	prompt("Here is your shopping list")
+	prompt("Here is your shopping list: ")
+	count = 1
 	for items in shopping_list:
-		prompt (items)
+		prompt("{}. {}".format(count,items))
+		count+=1
 
 def add_to_list(items):
 	shopping_list.append(items)
@@ -24,6 +34,12 @@ while True:
 
 	if new_items == "DONE":
 		break
+	elif new_items == "HELP":
+		show_help()
+		continue
+	elif new_items == "SHOW":
+		show_list()
+		continue
 	add_to_list(new_items.capitalize())
 	continue
 
